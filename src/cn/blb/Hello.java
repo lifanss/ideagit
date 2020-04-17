1bb6801713dcb4f2138eeab89a7714f85b4d2fa3
insist <1697794459@qq.com>
2020-04-17 15:11:44 +0800

    第三次提交

--- src/cn/blb/Hello.java
+++ src/cn/blb/Hello.java
@@ -3,5 +3,6 @@ package cn.blb;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hahahahhahahahh!!!");
+        System.out.println("bbbbbbbbbbbbbbbbbbb");
     }
 }
