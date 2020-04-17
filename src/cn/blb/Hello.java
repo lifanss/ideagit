1a65c5b665f79bf6ff74845b5b334ebfe163890f
insist <1697794459@qq.com>
2020-04-17 15:16:24 +0800

    开发扫码功能

--- src/cn/blb/Hello.java
+++ src/cn/blb/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hahahahhahahahh!!!");
         System.out.println("bbbbbbbbbbbbbbbbbbb");
+        System.out.println("saomiao");
     }
 }
